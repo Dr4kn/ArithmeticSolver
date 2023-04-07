bb40cf4ec1e25d762d24b6b416e8da844fbb65c3
Dr4kn <servicesDrkn@gmail.com>
2023-04-07 17:59:28 +0200

    refactored test to use hamcrest for better test failed messages

--- src/test/kotlin/arithmeticOperatorSolver/ArithmeticOperatorsTest.kt
+++ src/test/kotlin/arithmeticOperatorSolver/ArithmeticOperatorsTest.kt
@@ -1,159 +1,114 @@
 package arithmeticOperatorSolver
 
 import jdk.jfr.Description
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers
 import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.*
+import java.math.BigDecimal
 import java.math.MathContext
 
 class ArithmeticOperatorsTest {
     private val mathContext = MathContext.UNLIMITED
 
+    private fun calculate(number1: Number, number2: Number, operator: ArithmeticOperators): BigDecimal {
+        val mathContext = MathContext.UNLIMITED
+        return calculate(number1, number2, operator, mathContext)
+    }
+
+    private fun calculate(
+        number1: Number,
+        number2: Number,
+        operator: ArithmeticOperators,
+        mathContext: MathContext
+    ): BigDecimal {
+        return calculationFromOperator(operator)
+            .invoke(
+                (number1).toBigDecimal(mathContext),
+                (number2).toBigDecimal(mathContext)
+            )
+    }
+
     @Test
     @Description("No simple rounding like done by an Int division should occur")
     fun calculationAccuracy() {
-        assertEquals(
-            (3.5).toBigDecimal(mathContext),
-            calculationFromOperator(ArithmeticOperators.DIVIDE)
-                .invoke(
-                    (14).toBigDecimal(mathContext),
-                    (4).toBigDecimal(mathContext)
-                )
-        )
+        val expected = (3.5).toBigDecimal(mathContext)
+        val actual = calculate(14, 4, ArithmeticOperators.DIVIDE)
+
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     @Description("No simple routing like done by an Int division should occur")
     fun calculationAccuracyForNegativeNumber() {
-        assertEquals(
-            (-3.5).toBigDecimal(mathContext),
-            calculationFromOperator(ArithmeticOperators.DIVIDE)
-                .invoke(
-                    (14).toBigDecimal(mathContext),
-                    (-4).toBigDecimal(mathContext)
-                )
-        )
+        val expected = (-3.5).toBigDecimal(mathContext)
+        val actual = calculate(14, -4, ArithmeticOperators.DIVIDE)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun behaviourForInfiniteQuotient() {
-        assertTrue(
-            (2).toBigDecimal(mathContext)
-                .compareTo(
-                    calculationFromOperator(ArithmeticOperators.DIVIDE)
-                        .invoke(
-                            (0.2).toBigDecimal(mathContext),
-                            (0.1).toBigDecimal(mathContext)
-                        )
-                ) == 0
-        )
+        val expected = (2).toBigDecimal(mathContext)
+        val actual = calculate(0.2, 0.1, ArithmeticOperators.DIVIDE)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun addingPrecision() {
-        assertEquals(
-            (0.3).toBigDecimal(mathContext),
-            calculationFromOperator(ArithmeticOperators.ADD)
-                .invoke(
-                    (0.2).toBigDecimal(mathContext),
-                    (0.1).toBigDecimal(mathContext)
-                )
-        )
+        val expected = (0.3).toBigDecimal(mathContext)
+        val actual = calculate(0.2, 0.1, ArithmeticOperators.ADD)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun subtractingPrecision() {
-        assertEquals(
-            (0.1).toBigDecimal(mathContext),
-            calculationFromOperator(ArithmeticOperators.SUBTRACT)
-                .invoke(
-                    (0.2).toBigDecimal(mathContext),
-                    (0.1).toBigDecimal(mathContext)
-                )
-        )
+        val expected = (0.1).toBigDecimal(mathContext)
+        val actual = calculate(0.2, 0.1, ArithmeticOperators.SUBTRACT)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
+
 
     @Test
     fun multiplyPrecision() {
-        assertTrue(
-            (7).toBigDecimal(mathContext)
-                .compareTo(
-                    calculationFromOperator(ArithmeticOperators.MULTIPLY)
-                        .invoke(
-                            (3.5).toBigDecimal(mathContext),
-                            (2).toBigDecimal(mathContext)
-                        )
-                ) == 0
-        )
+        val expected = (7).toBigDecimal(mathContext)
+        val actual = calculate(3.5, 2, ArithmeticOperators.MULTIPLY)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun sumHasMoreDecimalPlaces() {
-        val sum = calculationFromOperator(ArithmeticOperators.ADD)
-            .invoke(
-                (3.500).toBigDecimal(mathContext),
-                (2).toBigDecimal(mathContext)
-            )
-
-        assertTrue(
-            (5.500000).toBigDecimal(mathContext)
-                .compareTo(sum) == 0
-        )
+        val expected = (5.500000).toBigDecimal(mathContext)
+        val actual = calculate(3.500, 2, ArithmeticOperators.ADD)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun differenceHasMoreDecimalPlaces() {
-        val difference = calculationFromOperator(ArithmeticOperators.SUBTRACT)
-            .invoke(
-                (3).toBigDecimal(mathContext),
-                (2.5987).toBigDecimal(mathContext)
-            )
-
-        assertTrue(
-            (0.401300).toBigDecimal(mathContext)
-                .compareTo(difference) == 0
-        )
+        val expected = (0.401300).toBigDecimal(mathContext)
+        val actual = calculate(3, 2.5987, ArithmeticOperators.SUBTRACT)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun productHasMoreDecimalPlaces() {
-        val product = calculationFromOperator(ArithmeticOperators.MULTIPLY)
-            .invoke(
-                (3.512).toBigDecimal(mathContext),
-                (2.8).toBigDecimal(mathContext)
-            )
-
-        assertTrue(
-            (9.8336).toBigDecimal(mathContext)
-                .compareTo(product) == 0
-        )
+        val expected = (9.8336).toBigDecimal(mathContext)
+        val actual = calculate(3.512, 2.8, ArithmeticOperators.MULTIPLY)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun quotientHasMoreDecimalPlaces() {
-        val quotient = calculationFromOperator(ArithmeticOperators.DIVIDE)
-            .invoke(
-                (8.3).toBigDecimal(mathContext),
-                (5.263).toBigDecimal(mathContext)
-            )
-
-        assertTrue(
-            (1.577047311).toBigDecimal(mathContext)
-                .compareTo(quotient) == 0
-        )
+        val expected = (1.577047311).toBigDecimal(mathContext)
+        val actual = calculate(8.3, 5.263, ArithmeticOperators.DIVIDE)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 
     @Test
     fun maxDecimalPlaces() {
         // there isn't a right or wrong here just if it matches the max that is specified.
         // can be changed to a lower number for performance or a higher one if the increased precision is needed
-        val fraction = calculationFromOperator(ArithmeticOperators.DIVIDE)
-            .invoke(
-                (1).toBigDecimal(mathContext),
-                (3).toBigDecimal(mathContext)
-            )
-        assertTrue(
-            (0.333_333_333_3).toBigDecimal(mathContext)
-                .compareTo(fraction) == 0
-        )
+        val expected = (0.333_333_333_3).toBigDecimal(mathContext)
+        val actual = calculate(1, 3, ArithmeticOperators.DIVIDE)
+        assertThat(expected, Matchers.comparesEqualTo(actual))
     }
 }
